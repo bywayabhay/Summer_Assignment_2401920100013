090f1489fe48ac0a2ce0dcbac7201f25d3fec5bc
Abhay Singh <singhabhay2085@gmail.com>
2026-06-15 20:28:54 +0530

    Add Linked_List_Cycle.cpp for cycle detection
    Implement algorithm to detect cycle in a linked list using Floyd's Tortoise and Hare approach.

---
+++ Week_3/Day_1/Linked_List_Cycle.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+/*
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        // TC -> O(n)
+        // SC -> O(1)
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+};
+
+// Problem Link : https://leetcode.com/problems/linked-list-cycle/
