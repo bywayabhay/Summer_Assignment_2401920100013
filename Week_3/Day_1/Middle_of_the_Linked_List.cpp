9881eb2da51bff03ce316e94e43d7d6babf46936
Abhay Singh <singhabhay2085@gmail.com>
2026-06-15 20:31:03 +0530

    Add solution for finding middle of linked list
    Implement function to find the middle node of a linked list.

---
+++ Week_3/Day_1/Middle_of_the_Linked_List.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        // TC -> O(n)
+        // SC -> O(1)
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
+
+// Problem Link : https://leetcode.com/problems/middle-of-the-linked-list/
